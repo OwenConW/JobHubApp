0a55dc95556fb9bf52762c045ec4e4d14c4d9332
atrconjerex <atrconjerex@gmail.com>
2022-08-15 17:03:56 -0300

    Vamo vamo Argentina

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <>
-      <h4>Aguante Argentina, vamoss a x Qatar 2022 Messi!</h4>
+      <h4>Aguante Argentina, vamoss a x Qatar 2022!</h4>
     </>
   );
 }
