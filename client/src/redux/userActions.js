9868f2a28f9124096f91043a79a1d8e16b52cd7b
OwenConW <Patricios78owen@gmail.com>
2022-08-15 17:24:47 -0300

    Feat(client/redux) add the basic configuration from redux, reducer, store and the actions

---
+++ client/src/redux/userActions.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+import { getAllUsers, getUserById } from "./userSlice.js"
+
+export const getChars = () => (dispatch) => {
+    axios.get("/users")
+    .then(res => {
+        dispatch(getAllUsers(res.data))
+    })
+    .catch(e => console.log(e))
+}
+ 
+export const getCharsById = (id) => (dispatch) => {
+    axios.get("/users/:id")
+    .then(res => {
+        dispatch(getUserById(id))
+    })
+    .catch(e => console.log(e))
+}
