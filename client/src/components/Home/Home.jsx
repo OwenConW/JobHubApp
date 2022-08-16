93c80e3f4ad99f6e2adfc89514af37e67eb9d576
atrconjerex <atrconjerex@gmail.com>
2022-08-16 19:14:28 -0300

    UPDATE Home.jsx with styles

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,11 +4,12 @@ import React from 'react';
 import Navbar from "../Navbar/Navbar";
 
 //assets
-
+import mapa from './assets/mapa_fondo.svg';
 
 //styles and utilities
 import s from './Home.module.scss';
 import { Link } from 'react-router-dom';
+
 
 const Home = () => {
   return (
@@ -17,16 +18,14 @@ const Home = () => {
           <Navbar />
         </div>
         <div className={s.presentation}>
-            <h1 className={s.title}>Titulo canchero llamativo</h1>
-            <h3 className={s.text}>Aqui en nuestra plataforma, podrás encontrar los mejores profesionales para satisfacer tus necesidades</h3>
-            <h3 className={s.text}>Encuentra a l@s mejores trabajador@s buscándolos por catálogo, cerca de tu área, contactá, llegá a un acuerdo y listo</h3>
-            <h3 className={s.text}>Tenemos las mejores herramientas para que encuentres a las personas adecuadas, rápido, sencillo y cerca de tu cuidad.</h3>
+            <h1 className={s.title}>El buen servicio de un profesional está a tu alcance!</h1>
+            <h3 className={s.text}>Tenemos las mejores herramientas para que encuentres a las personas adecuadas en tu cuidad actual, rápido, seguro y fácil.</h3>
+            <h3 className={s.text}>Encuentra a l@s mejores trabajador@s buscándolos por catálogo,</h3>
+            <h3 className={s.text}>cerca de tu área, contactá, llegá a un acuerdo y listo.</h3>
         </div>
         <div className={s.destacados}>
-            <h1 className={s.title}>Trabajadores destacados de la semana</h1>
-            
+            <h1 className={s.subtitle}>Trabajadores destacados de la semana</h1>
             {/* {Aqui van las 3 cards de los destacados!!!!} */}
-            
             <Link to='/professionals' className={s.link}>
                 <button className={s.button}>Explorar Catálogo</button>
             </Link>
