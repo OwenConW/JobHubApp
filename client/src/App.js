93316373273c8eee202b516d294467ee1a5ec6fc
OwenConW <Patricios78owen@gmail.com>
2022-08-16 12:17:48 -0300

    restart app

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,6 @@ import React from "react";
 function App() {
   return (
     <>
-    <h3>Aguente boqueeee</h3>
-    <h3>hola</h3>
-    <h1>Bueno a ver como lo arreglo</h1>
-    <form>
-      <label>NAshe:</label>
-      <input type="text"></input>
-    </form>
-    <h1>OWen</h1>
-
     </>
   );
 }
