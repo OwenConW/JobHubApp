34957452ef4259a503c67440e0d0d62efb3c2eb4
Your Namedavi <davidgutierrezcontacto@gmail>
2022-08-16 17:08:22 -0300

    david: modified app/src/models/Client.js

--- api/src/models/Client.js
+++ api/src/models/Client.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define('cliente', {
+    sequelize.define('client', {
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
