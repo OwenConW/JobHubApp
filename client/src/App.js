3c07d8183da88e471eb5fc64c5ff68be36726270
atrconjerex <atrconjerex@gmail.com>
2022-08-16 13:03:15 -0300

    jere add h1 probandoo

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import React from "react";
 function App() {
   return (
     <>
-    <h3>Aguente boqueeee</h3>
-    <h3>hola</h3>
+      <h3>Aguente boqueeee</h3>
+      <h3>hola</h3>
+      <h1>probandoooo</h1>
     </>
   );
 }
