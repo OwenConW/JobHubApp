00dcfa4887fe7b0357d20cad97ef9d6e5481aa68
aleema95 <alejandro.manouellian@gmail.com>
2022-08-17 16:50:27 -0300

    created state in Catalog component to store filter values

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import Filter from './Filter/Filter';
+import { useEffect } from 'react';
 
 const Catalog = (props) => {
 
+const [filters, setFilters] = useState() 
+
 function addFilterValue(targetName, value){
 	console.log('targetName:',targetName,"| Value:", value);
+	setFilters(prevState => ({
+		...prevState,
+		[targetName]: value
+	}))
 }
+
+useEffect(() => {
+	console.log(filters);
+}, [filters])
 
 	return (
 		<div>
