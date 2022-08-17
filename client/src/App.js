61bf7a1d291b40e654fe658ccc648ff687dd0dc1
Mona Lisa <valentinojesuss4@gmail.com>
2022-08-17 16:35:09 -0300

    modified(client/src/components/Login) Changes in Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css';
+import Login from "./components/Login/Login";
 
 import Navbar from "./components/Navbar/Navbar";
 
+//Limpiar antes del pull
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Login/>
     </div>
   );
 }
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 //style and utilities
 import s from './Login.module.scss';
@@ -10,7 +11,16 @@ const Login = () => {
     <div className={s.container}>
         <div className={s.login}>
             <div className={s.logo}>
+                <img src={logo} alt="logo" />
+            </div>
+            <div className={s.login_btn}>
+                <p>Iniciar Sesión</p>
+            </div>
 
+            <div className={s.invite}>
+                <Link to='/home' className={s.link}>
+                    <p>Continuar como invitado</p>
+                </Link>
             </div>
         </div>
         <div className={s.description}>
