177714c5b4c08c281ed73a65912849039bf16da0
scottimariano <32655578+scottimariano@users.noreply.github.com>
2022-08-17 16:06:18 -0300

    Catalog updated, waiting filters and cards

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import SearchBar from './SearchBar/SearchBar';
+import estilos from './Catalog.module.scss';
 
 const Catalog = (props) => {
 	return (
-		<div>
-			CATALOGO
-			<SearchBar />
+		<div className={estilos.container}>
+			<aside className={estilos.aside}>
+				<div>
+					<SearchBar />
+				</div>
+				<div>FILTROS!!!</div>
+			</aside>
+			<div>
+				<header className={estilos.header}>
+					<span>Catálogo de profesionales</span>
+				</header>
+				<div className={estilos.cardsContainer}>
+					<div>CARDS</div>
+					<div>CARDS</div>
+					<div>CARDS</div>
+					<div>CARDS</div>
+					<div>CARDS</div>
+					<div>CARDS</div>
+				</div>
+			</div>
 		</div>
 	);
 };
