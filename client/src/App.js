cfc77027985e608ab7df6f66670991e25f6783e1
scottimariano <32655578+scottimariano@users.noreply.github.com>
2022-08-17 12:24:32 -0300

    Catalogo added in App with Catalog name fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
-import Catalogo from './components/Catalogo/Catalogo';
+import Catalog from './components/Catalog/Catalog';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<Catalogo />
+			<Catalog />
 		</div>
 	);
 }
