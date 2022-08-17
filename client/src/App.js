8db2b1a3c82c436b5ddac4c0b69b6ce5a1546cb5
scottimariano <32655578+scottimariano@users.noreply.github.com>
2022-08-17 12:19:57 -0300

    Catalogo added in App

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
-import SearchBar from './components/SearchBar/SearchBar';
+import Catalogo from './components/Catalogo/Catalogo';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<SearchBar />
+			<Catalogo />
 		</div>
 	);
 }
