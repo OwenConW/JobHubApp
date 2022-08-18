2edf848861a6e53c3480e3cd047947be18897941
scottimariano <32655578+scottimariano@users.noreply.github.com>
2022-08-18 15:28:16 -0300

    Cars conectadas con el back

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { filterProfessionals } from '../../redux/userActions';
 import SearchBar from './SearchBar/SearchBar';
 import Filter from './Filter/Filter';
 import estilos from './Catalog.module.scss';
 import Card from '../Card/Card';
 import Navbar from '../Navbar/Navbar';
+import { getChars } from '../../redux/userActions';
 
 const Catalog = (props) => {
+	let professionalsArray = useSelector(
+		(state) => state.users.filteredProfessionals
+	);
+
 	const [filters, setFilters] = useState({
 		name: '',
 		profession: '',
@@ -15,6 +20,11 @@ const Catalog = (props) => {
 	});
 
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getChars());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	function addFilterValue(targetName, value) {
 		if (targetName === 'rating' && filters.rating === 'ASC') {
@@ -33,15 +43,18 @@ const Catalog = (props) => {
 	function handleSubmit(e) {
 		e.preventDefault();
 		dispatch(filterProfessionals({ ...filters }));
+		e.target.reset();
 	}
 
+	function handleShowAll() {
+		dispatch(getChars());
+	}
 	return (
 		<>
 			<Navbar />
 			<div className={estilos.container}>
 				<aside className={estilos.aside}>
 					<form onSubmit={handleSubmit}>
-						CATALOGO
 						<SearchBar addFilterValue={addFilterValue} />
 						<Filter addFilterValue={addFilterValue} />
 						<input
@@ -50,21 +63,29 @@ const Catalog = (props) => {
 							value="SEARCH"
 						/>
 					</form>
+					<button onClick={handleShowAll}>SHOW ALL</button>
 				</aside>
 				<div className={estilos.professionals}>
 					<header className={estilos.header}>
 						<span>Catálogo de profesionales</span>
 					</header>
 					<div className={estilos.cardsContainer}>
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-						<Card />
+						{professionalsArray && professionalsArray.length ? (
+							professionalsArray.map((p, i) => (
+								<Card
+									key={i}
+									data={{
+										...p,
+										image: 'https://api.time.com/wp-content/uploads/2017/12/terry-crews-person-of-year-2017-time-magazine-2.jpg', //modificar cuando este en la DB!!
+									}}
+								/>
+							))
+						) : (
+							<div>
+								-- NO ENCONTRAMOS PROFESIONALES QUE SE AJUSTEN A TU
+								BUSQUEDA --
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
