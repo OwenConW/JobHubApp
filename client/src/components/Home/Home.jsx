a9084cadf867e7d6d47ac88591b856fc11559ec0
atrconjerex <atrconjerex@gmail.com>
2022-08-18 14:45:20 -0300

    UPDATE Home styles containers

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -15,31 +15,33 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className={s.container1}>
-        <div className={s.presentation}>
-          <h1 className={s.title}>El buen servicio de un profesional está a tu alcance!</h1>
-          <h3 className={s.text}>Tenemos las mejores herramientas para que encuentres a las personas adecuadas en tu cuidad actual, rápido, seguro y fácil.</h3>
-          <h3 className={s.text}>Encuentra a l@s mejores trabajador@s buscándolos por catálogo,</h3>
-          <h3 className={s.text}>cerca de tu área, contactá, llegá a un acuerdo y listo.</h3>
+      <div className={s.container}>
+        <aside className={s.aside}>
+          <div className={s.presentation}>
+            <h1 className={s.title}>El buen servicio de un profesional está a tu alcance!</h1>
+            <h3 className={s.text}>Tenemos las mejores herramientas para que encuentres a las personas adecuadas en tu cuidad actual, rápido, seguro y fácil.</h3>
+            <h3 className={s.text}>Encuentra a l@s mejores trabajador@s buscándolos por catálogo,</h3>
+            <h3 className={s.text}>cerca de tu área, contactá, llegá a un acuerdo y listo.</h3>
+          </div>
+          <div className={s.destacados}>
+            <h1 className={s.subtitle}>Trabajadores destacados de la semana</h1>
+            {/* {Aqui van las 3 cards de los destacados!!!!} */}
+            <Link to='/professionals' className={s.link}>
+              <button className={s.button}>Explorar Catálogo</button>
+            </Link>
+          </div>
+        </aside>
+        <div className={s.container2}>
+          <h1 className={s.titlemapa}>Utiliza nuestro sistema de búsqueda por ubicación</h1>
+          {isAutenticated ?
+            <Link to='/map' className={s.link}>
+              <button className={s.button}>Buscar</button> 
+            </Link>
+          : <Link to='/login' className={s.link}>
+              <button className={s.button}>Iniciar Sesión</button> 
+            </Link>
+          }
         </div>
-        <div className={s.destacados}>
-          <h1 className={s.subtitle}>Trabajadores destacados de la semana</h1>
-          {/* {Aqui van las 3 cards de los destacados!!!!} */}
-          <Link to='/professionals' className={s.link}>
-            <button className={s.button}>Explorar Catálogo</button>
-          </Link>
-        </div>
-      </div>
-      <div className={s.container2}>
-        <h1 className={s.titlemapa}>Utiliza nuestro sistema de búsqueda por ubicación</h1>
-        {isAutenticated ?
-          <Link to='/map' className={s.link}>
-            <button className={s.button}>Buscar</button> 
-          </Link>
-        : <Link to='/login' className={s.link}>
-            <button className={s.button}>Iniciar Sesión</button> 
-          </Link>
-        }
       </div>
     </>    
   )
