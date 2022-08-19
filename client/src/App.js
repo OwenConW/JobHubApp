4c9ff5414379de4553e6235e40680d794a95883c
OwenConW <Patricios78owen@gmail.com>
2022-08-19 20:14:08 -0300

    owen add(client/components/uploadimage) agregue la funcionalidad que le permite al usuario ingresar una foto desde su sistema

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import Catalog from './components/Catalog/Catalog';
 import Map from './components/Map/Map';
-import UploadImage from "./components/uploadImage/UploadImage"
+
 import Details from './components/Details/Details'
 import Onboarding from "./components/Onboarding/Onboarding";
 
@@ -15,7 +15,15 @@ import Onboarding from "./components/Onboarding/Onboarding";
 function App() {
 	return (
 		<div className="App">
-			<UploadImage></UploadImage>
+			<Routes>
+				<Route exact path="/" element={<Login />} />
+				<Route exact path="/home" element={<Home />} />
+				<Route exact path="/professionals" element={<Catalog />} />
+				<Route exact path="/map" element={<Map />} />
+        		<Route exact path="/profile" element={<Profile />}/>
+				<Route exact path="/details/:id" element={<Details />}/>
+				<Route exact path="/onboarding" element={<Onboarding />}/>
+			</Routes>
 		</div>
 	);
 }
