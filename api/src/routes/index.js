4fd37124e8b726ecc5d3dbe1bddf84fc6c8c1fbb
OwenConW <Patricios78owen@gmail.com>
2022-08-19 14:47:33 -0300

    owen add(api/routes/mails) cree la funcionalidad en una ruta de poder mandar un mail

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const users = require("./users/users.route")
 const verify = require("./verify/verify.route")
 const jobs = require("./jobs/jobs.route")
+const mails = require("./nodemailer/nodemailer.route")
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 router.use("/users", users);
 router.use("/verify", verify);
 router.use("/jobs", jobs);
+router.use("/mails", mails)
 // router.use("/x", x)
 
 // Configurar los routers
