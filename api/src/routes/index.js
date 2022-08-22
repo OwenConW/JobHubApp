22b3a25f4fd6d310afaf811f08c1fe7f824b3335
OwenConW <Patricios78owen@gmail.com>
2022-08-22 16:12:47 -0300

    owen add(api/routes/pago and api/service and api/functions) agregue la pasarela de pago anashe

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,6 +3,7 @@ const users = require("./users/users.route")
 const verify = require("./verify/verify.route")
 const jobs = require("./jobs/jobs.route")
 const mails = require("./nodemailer/nodemailer.route")
+const pagos = require("./pagos/pagos")
 //const review = require('./review/review.route');
 
 // Importar todos los routers;
@@ -13,6 +14,7 @@ router.use("/users", users);
 router.use("/verify", verify);
 router.use("/jobs", jobs);
 router.use("/mails", mails)
+router.use("/pagos", pagos)
 // router.use("/review", review);
 
 
