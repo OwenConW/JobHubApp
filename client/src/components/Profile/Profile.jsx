a618e13e03e564ca5fdaacafeae2dbf097651e81
Martin Morales <89616378+MartinMorales1504@users.noreply.github.com>
2022-08-24 20:35:09 -0300

    Update Profile.jsx

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -15,24 +15,6 @@ import axios from "axios";
 
 
 
-
-//ESTADO HARCODEADO PARA HACER PRUEBAS EN PROFILE
-// let activeUser = {
-//   name: "lionel test nuevo",
-//   last_Name: "messi",
-//   description: "hola mi nombre es lio messi trucho y esto es disney CHANNEL",
-//   mail: "test2@gmail.com",
-//   dni: "83.332.125",
-//   image: "not image",
-//   phone: "1656158172",
-//   country: "Rusia",
-//   // postal_code: "1406",
-//   city: "Moscu",
-//   coordinate: ["421", "-22"],
-//   professions: [{ name: "extraterrestre" }, { name: "sovietico" }, { name: "militar" }, { name: "armamentista" }, { name: "electricista" }, { name: "gasista" }, { name: "programador" }],
-//   isPremium: false
-// }
-
 const Profile = () => {
 
   let activeUser = getLocalStorage();
