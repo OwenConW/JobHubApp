c792f624612c04349ccc594e838e68261f3a63be
OwenConW <Patricios78owen@gmail.com>
2022-08-26 16:35:02 -0300

    fix the chat and mp :D

--- api/src/routes/pagos/pagos.js
+++ api/src/routes/pagos/pagos.js
@@ -5,13 +5,6 @@ const PaymentService = require("../../Services/PaymentService")
 const PaymentInstance = new PaymentController(new PaymentService())
 const pagos = Router()
 
-// SDK de Mercado Pago
-const mercadopago = require("mercadopago");
-// Agrega credenciales
-mercadopago.configure({
-  access_token: process.env.ACCESS_TOKEN,
-});
-
 pagos.post("/premium", (req, res, next) => {
     PaymentInstance.getSubscriptionLink(req, res)
 })
