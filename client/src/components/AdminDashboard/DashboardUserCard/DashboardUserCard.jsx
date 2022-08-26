16b1bbb3c053553531bbfd9583a11e6ab89f6c39
aleema95 <alejandro.manouellian@gmail.com>
2022-08-25 22:11:28 -0300

    commit porque si

--- client/src/components/AdminDashboard/DashboardUserCard/DashboardUserCard.jsx
+++ client/src/components/AdminDashboard/DashboardUserCard/DashboardUserCard.jsx
@@ -27,27 +27,53 @@ function DashboardUserCard(props) {
 
   return (
     <div className={s.cardContainer}>
-      <h1>Nombre: {name} {last_Name}</h1>
-      <p>Description: {description}</p>
-      <h4>DNI: {dni}</h4>
-      <h4>Profesional: Si{isProfessional}</h4>
-      <h4>Fecha de nacimiento: 17/03/1999{date_of_Birth}</h4>
-      <img src={image} alt="profile-img" />
-      <h4>Email: {mail}</h4>
-      <h4>Telefono: {phone}</h4>
-      <h4>Pais: {country}</h4>
-      <h4>Ciudad: {city}</h4>
-      <h4>Rating: {rating}</h4>
-      <h4>Premium: Activo{isPremium}</h4>
-      <h4>Admin: Si{isAdmin}</h4>
-      <h4>Suspendido: Si{isBanned}</h4>
-      <h4>Activo: Si{isActive}</h4>
-      <h4>Profesiones: {professions?.map(p => {
-        return (
-          <h4>{p.name}</h4>
-        )
-      })}
-      </h4>
+      <div className={s.importantInformationContainer}>
+        <div className={s.nameContainer}>
+          <h1>Nombre</h1>
+          <h1>{name} {last_Name}</h1>
+        </div>
+        <div>
+          <h4>Premium</h4>
+          <h4>Activo{isPremium}</h4>
+        </div>
+        <div>
+          <h4>Admin</h4>
+          <h4>Si{isAdmin}</h4>
+        </div>
+        <div>
+          <h4>Activo</h4>
+          <h4>Si{isActive}</h4>
+        </div>
+        <div>
+          <h4>Profesional</h4>
+          <h4>Si{isProfessional}</h4>
+        </div>
+        <div>
+          <h4>Rating</h4>
+          <h4>{rating}</h4>
+        </div>
+        <div>
+          <h4>Suspendido</h4>
+          <h4> Si{isBanned}</h4>
+        </div>        
+      </div>
+      {/* Division */}
+      <div>
+        <h4>DNI: {dni}</h4>
+        <h4>Fecha de nacimiento: 17/03/1999{date_of_Birth}</h4>
+        <img src={image} alt="profile-img" />
+        <h4>Email: {mail}</h4>
+        <h4>Telefono: {phone}</h4>
+        <h4>Pais: {country}</h4>
+        <h4>Ciudad: {city}</h4>
+        <p>Description: {description}</p>
+        <h4>Profesiones: {professions?.map(p => {
+          return (
+            <h4>{p.name}</h4>
+          )
+        })}
+        </h4>
+      </div>
     </div>
   )
 }
