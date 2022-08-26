532193b14b5284c65d6b76be77c2614e7f6e81f1
Mona Lisa <valentinojesuss4@gmail.com>
2022-08-26 18:51:28 -0300

    prev to pull development

--- client/src/components/LiveChat/Chat/Chat.jsx
+++ client/src/components/LiveChat/Chat/Chat.jsx
@@ -39,7 +39,7 @@ const Chat = (props) => {
 
 
     useEffect(() => {
-        socket.current = io("ws://https://jobhub-pg.herokuapp.com"); //"ws://localhost:3001"
+        socket.current = io("ws://localhost:3001"); //"ws://https://jobhub-pg.herokuapp.com"
         socket.current.on("getMessage", data => {
             setArriveMessage({
                 sender: data.senderId,
