5b10ea79c119e7c753d34f07da652aa4cc99fe60
OwenConW <Patricios78owen@gmail.com>
2022-08-26 14:56:15 -0300

    to master

--- api/src/app.js
+++ api/src/app.js
@@ -38,16 +38,10 @@ app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 
 const server = http.createServer(app, {
   cors: {
-    methods: ["GET", "POST"]
+    origin: "https://jobhub.vercel.app"
   }
 });
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-})
+
 const io = socketio(server, {
 
     cors: {
