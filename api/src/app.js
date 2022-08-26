4496a69d97721342737eec5ad054b93b5938eae1
OwenConW <Patricios78owen@gmail.com>
2022-08-26 14:45:32 -0300

    to master

--- api/src/app.js
+++ api/src/app.js
@@ -37,7 +37,6 @@ app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 });
 
 const server = http.createServer(app);
-
 const io = socketio(server, {
 
     cors: {
@@ -88,6 +87,14 @@ io.on("connection", socket => {
       removeUser(socket.id)
       io.emit("getUsers", users)
     })
+})
+
+io.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 })
 
 module.exports = server;
