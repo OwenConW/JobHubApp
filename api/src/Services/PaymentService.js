d5788d61f1a2be76719db756546683c241c6c2c3
OwenConW <Patricios78owen@gmail.com>
2022-08-27 11:02:12 -0300

    modified paymentservice

--- api/src/Services/PaymentService.js
+++ api/src/Services/PaymentService.js
@@ -13,7 +13,7 @@
           transaction_amount: 4000,
           currency_id: "ARS"
         },
-        back_url: "https://jobhub-pg.herokuapp.com/profile/success",
+        back_url: "https://jobhub-pg.herokuapp.com/profile",
         payer_email: `${mail}`
       };
   
