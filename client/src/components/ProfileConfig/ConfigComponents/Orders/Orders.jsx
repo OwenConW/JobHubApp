236d1bd2e5e400408aa4eedb2b5b2a7c6c2a7f0f
atrconjerex <atrconjerex@gmail.com>
2022-08-27 16:10:11 -0300

    UPDATED styles and orders cards finished

--- client/src/components/ProfileConfig/ConfigComponents/Orders/Orders.jsx
+++ client/src/components/ProfileConfig/ConfigComponents/Orders/Orders.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import OrderCard from './OrderCard/OrderCard';
+import s from './Orders.module.scss';
 
 const Orders = (props) => {
   return (
     <>
-      <OrderCard />
-      <OrderCard />
-      <OrderCard />
+      <div className={s.container}>
+        <h1>Mis Ordenes</h1>
+        <div className={s.cardsContainer}>
+          <OrderCard />
+          <OrderCard />
+          <OrderCard />
+        </div>
+      </div>
     </> 
   );
 }
