c3a4e08759ca8c6c4790eb40fa380b53a14f1b22
OwenConW <Patricios78owen@gmail.com>
2022-08-28 18:25:56 -0300

    add more routes to send emails

--- api/src/routes/nodemailer/nodemailer.route.js
+++ api/src/routes/nodemailer/nodemailer.route.js
@@ -37,7 +37,6 @@ mails.get("/premiumspam", (req, res, next) => {
 
 mails.get("/welcome", (req, res, next) => {
     const { mail, name} = req.query
-    console.log("datos:", mail, name)
     transporter.sendMail({
         from: `"Job Hub App" <jobhub@gmail.com>`,
         to: mail,
@@ -45,7 +44,15 @@ mails.get("/welcome", (req, res, next) => {
         html: `
         <h1>Hola ${name}!!!</h1>
         <br></br>
-        
+        <h2>Te agradecemos por navegar por nuestra página web!</h2>
+        <h2>Recorda que podes publicar tus empleos y contactar con empleadores a $0 costo!</h2><br></br>
+        <h2>Si quieres colaborar y formar parte de la familia JobHub recorda que podes acceder a la membresia premium y a todas sus ventajas</h2>
+        <h2>por el costo de ARS$4000 por un año!!</h2>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <b>©JobHubApp 2022</b>
         `
     })
@@ -56,6 +63,39 @@ mails.get("/welcome", (req, res, next) => {
         console.log(e)
         return res.status(404).send(e)
     }) 
+})
+
+
+mails.get("/bienvenido/premium", (req, res, next) => {
+    const { mail, name} = req.query
+    transporter.sendMail({
+        from: `"Job Hub App" <jobhub@gmail.com>`,
+        to: mail,
+        subject: "Muchas gracias por unirte a la familia JobHub!",
+        html: `
+        <h1 style="text-transform: capitalize; display: inline">¡¡${name}</h1><h1 style="display: inline"> bienvenido a la familia!!</h1>
+        <br></br>
+        <h2>Estamos muy contentos y agradecidos con que te hayas unido a nuestro bando!</h2>
+        <h2>Recorda que tendras acceso a todos los beneficios durante 1 año!</h2><br></br>
+        <h2>Y aun hay mas, la siguiente vez si renuevas tu suscripcion costara tan solo ARS$3000 por haber pasado todo un año compartiendo nuestros ideales.</h2>
+        <h2>Esperamos que podamos ayudarte a conseguir mucho trabajo y te volvemos a agradecer ${name}!</h2>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <b>©JobHubApp 2022</b>
+        `
+    })
+    .then(() => {
+        return res.send(`Mail enviado con éxito a ${mail}`)
+    })
+    .catch(e => {
+        console.log(e)
+        return res.status(404).send(e)
+    }) 
+
+
 })
 
 
