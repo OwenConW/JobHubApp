aced17c1fbfefcc87c1053d6c185861d9cf1b251
atrconjerex <atrconjerex@gmail.com>
2022-08-29 22:17:01 -0300

    ADD MIU Paginate

--- client/src/components/Catalog/Paginate/Paginate.jsx
+++ client/src/components/Catalog/Paginate/Paginate.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import s from './Paginate.module.scss';
 
 export default function Paginate({ proffesionalsPerPage, professionalsArray, paginado, currentPage }) {
