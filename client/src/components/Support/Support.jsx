e09b6d0276570908daa8430d8bb129d88cda8868
atrconjerex <atrconjerex@gmail.com>
2022-09-01 15:40:05 -0300

    Create & ADD Support to App.js

---
+++ client/src/components/Support/Support.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import s from './scss/Support.module.scss';
+
+const Support = () => {
+
+  return (
+    <>
+      <div className={s.container}>
+        <div className={s.component}>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Support;
