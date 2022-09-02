0a1851444eba5634ca7a6ab0a3207aadccb2b848
atrconjerex <atrconjerex@gmail.com>
2022-09-02 17:04:43 -0300

    ADD styles to Support

--- client/src/components/Support/Support.jsx
+++ client/src/components/Support/Support.jsx
@@ -11,7 +11,8 @@ function validate(msgSupport) {
   let errors = {};
   if (msgSupport.subject.length === 0) {
     errors.subject = "Por favor seleccione un asunto";
-  } else if (!msgSupport.description) {
+  } 
+  if (!msgSupport.description) {
     errors.description = "Por favor déjenos una descripción de su problema";  
   } 
   return errors;
@@ -44,7 +45,7 @@ const Support = () => {
     name: name ? name : "",
     last_Name: last_name ? last_name : "",
     // userIDreported: userIDreported && userIDreported.id || "",
-    subject: [],
+    subject: "",
     description: ""
   })
 
@@ -54,46 +55,39 @@ const Support = () => {
       ...msgSupport,
       [e.target.name]: e.target.value
     })
-    setError(                          
-      validate({
-        ...msgSupport,
-        [e.target.name]: e.target.value,  
-      })
-    )
   }
 
   function handleSelect(e) {
     e.preventDefault()
-    if(!msgSupport.subject.includes(e.target.value) && e.target.value !== 'vacio') {
+    if(asuntos.includes(e.target.value) && e.target.value !== 'vacio') {
       setMsgSupport({
         ...msgSupport,
-        subject: [...msgSupport.subject, e.target.value]
+        subject: e.target.value
       })
     }
-
-    e.target.value = 'vacio';
-    setError(                          
-      validate({
-        ...msgSupport,
-        [e.target.name]: e.target.value,  
-      })
-    );
   }  
   
-  function handleSubmit(e) {  
-    if(Object.keys(errors).length === 0) { 
+  function handleSubmit(e) {
+    if(msgSupport.subject == "" || msgSupport.description == "") {
+      setError(                          
+        validate({
+          ...msgSupport,
+          [e.target.name]: e.target.value  
+        })
+      )
+    } else {
       e.preventDefault()
       alert('Gracias, hemos recibido su problema!')
       setMsgSupport({
         userID: "",
         name: "",
         last_Name: "",
-        userIDreported: "",                          
+        // userIDreported: "",                          
         subject: [],
         description: ""
       })
       navigate("../home", { replace: true });
-    }
+    }  
     e.preventDefault()
   }
 
@@ -103,28 +97,27 @@ const Support = () => {
         <div className={s.component}>
           <form className={s.form}  onSubmit={(e) => handleSubmit(e)}>
             <Link to='/home'><button className={s.button}>Volver al Inicio</button></Link>
-            <div>
-              <h1 id={s.title}>ENVÍANOS TU PROBLEMA</h1>
+            <div className={s.contain}>
+              <h1 id={s.title}>ENVÍANOS TU CONSULTA</h1>
               <h2 id={s.subtitle}>Un admin se contactará contigo en la brevedad... </h2>
+              <h2 id={s.select}>Selecciona el asunto de tu problema:</h2>
+              <select className={s.subjects} value={msgSupport.subject} onChange={(e) => handleSelect(e)}>
+                <option hidden selected value='vacio'>Elige un asunto...</option>
+                {asuntos?.map( a => {
+                  return (<option key={a} value={a}>{a}</option>)
+                })}
+              </select>
+              {errors.subject && <p className={s.err}>{errors.subject}</p>}
             </div>
-            <h3>Selecciona el asunto de tu problema:</h3>
-            <select className={s.subjects} required onChange={(e) => handleSelect(e)}>
-              <option hidden selected value='vacio'>Elige un asunto...</option>
-              {asuntos?.map((a, index) => (
-                 <option key={index} value={a}>{a}</option>
-              ))}
-            </select>
-            {errors.subject && <p className={s.err}> {errors.subject}</p>}
-            <div>
+            <div className={s.contain2}>
               <h2>Descripción:</h2>
               <textarea className={s.description}
                 type="text"
                 value={msgSupport.description}
-                required
                 name="description"
                 onChange={(e) => handleChange(e)}
               />
-              {errors.description && <p className={s.err}> {errors.description}</p>}
+              {errors.description && <p className={s.err}>{errors.description}</p>}
             </div>
             <button className={s.buttonCreate} type="submit">Enviar</button>
           </form>    
